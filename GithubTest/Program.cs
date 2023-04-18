ba4c4c744c68d79c1963774ae773a0fd39186d36
Archer Harmony <clumsyninja007@gmail.com>
2023-04-18 14:07:55 -0400

    add basic hello world console app

---
+++ GithubTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.Write("Press enter to close...");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
